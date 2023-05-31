bdf1595cec101c4eea4150d4137e6103396f83eb
nicolas <nicolas.lopez1919@gmail.com>
2023-05-31 11:37:25 -0300

    footer-algunosEstilos

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+
+//eventos del Menu hamburguesa
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
-
 abrir.addEventListener("click", () =>{
     nav.classList.add("visible");
 })
-
 cerrar.addEventListener("click", () =>{
     nav.classList.remove("visible");
 })
-
 const titulo1 = document.getElementById('titulo')
-
 console.log(titulo1);
+
+
+
+
+
+
+
